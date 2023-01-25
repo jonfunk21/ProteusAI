1881751ebaf27683270395d6c4c1e0141fe322df
jonfunk21 <funk.jonathan21@gmail.com>
2023-01-25 10:15:42 +0100

    changes to embedd.py

--- extraction/embedd.py
+++ extraction/embedd.py
@@ -1,15 +1,8 @@
 import torch
 import esm
 import os
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device)
-
-# Load ESM-2 model
-model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
-model.to(device)
-model.eval()  # disables dropout for deterministic results
-batch_converter = alphabet.get_batch_converter()
+import time
+import argparse
 
 def compute_representations(data: tuple, dest: str = None, device: str = 'cuda'):
     '''
@@ -90,18 +83,57 @@ def batch_fasta(fasta_path: str, batch_size: int):
 
         return batches, _activities
 
-FASTA_PATH = "../esm/examples/data/P62593.fasta"
-DEST = "../example_data/representations/P62593"
-if not os.path.exists(DEST):
-    os.makedirs(DEST)
-BATCH_SIZE=26
+def embedd(fasta_path:str, dest:str, batch_size):
 
-import time
-start_time = time.time()
-batches, activities = batch_fasta(fasta_path=FASTA_PATH, batch_size=BATCH_SIZE)
-for batch in batches:
-    seq_rep = compute_representations(batch, dest=DEST, device=device)
-end_time = time.time()
-elapsed_time = end_time - start_time
+    if not os.path.exists(dest):
+        os.makedirs(dest)
 
-print(elapsed_time)
+    start_time = time.time()
+    batches, activities = batch_fasta(fasta_path=fasta_path, batch_size=batch_size)
+    for batch in batches:
+        seq_rep = compute_representations(batch, dest=dest, device=device)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(elapsed_time)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=
+     '''
+     Description:
+     Creates embeddings for a fasta file using esm.pretrained.esm2_t33_650M_UR50D() 
+     and saves them at the destination.
+     
+     Fasta file format:
+     >{index}|{id}|{activity}
+     {Sequence}
+     
+     saves files as:
+     <path to dest>/{id}.pt
+     
+     example:
+     python3 embedd.py -f input.fasta -d out_dir -b 16
+     ''',
+    formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('-f', '--fasta', help='path to fasta file', default='../esm/examples/data/P62593.fasta')
+    parser.add_argument('-d', '--dest', help='path to destination', default='../example_data/representations/P62593')
+    parser.add_argument('-b', '--batch_size', help='batch_size', default=26)
+    args = parser.parse_args()
+
+
+    FASTA_PATH = args.fasta
+    DEST = args.dest
+    BATCH_SIZE= args.batch_size
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+
+    # Load ESM-2 model
+    model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
+    model.to(device)
+    model.eval()  # disables dropout for deterministic results
+
+    batch_converter = alphabet.get_batch_converter()
+
+    embedd(FASTA_PATH, DEST, BATCH_SIZE)
