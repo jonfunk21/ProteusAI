9116073773d373171f84bae8d645fde4699c0da8
jonfunk21 <funk.jonathan21@gmail.com>
2023-02-08 16:00:40 +0100

    embedd methyl transferases

--- scripts/test.py
+++ scripts/test.py
@@ -71,7 +71,7 @@ def compute_representations(data: list, dest: str = None, device: str = 'cuda'):
 
     return sequence_representations
 
-data = list(zip(hits, seqs))
+data = list(zip(names, seqs))
 print(len(data))
 for i in range(0, len(data), batch_size):
     r = compute_representations(PNMT_data[i:i + batch_size], dest=dest ,device=str(device))
