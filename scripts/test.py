5148e1fd7bfb6e410eeeec506618c3337ffe7a3b
jonfunk21 <funk.jonathan21@gmail.com>
2023-02-08 09:34:14 +0100

    compute representations on HPC

--- scripts/test.py
+++ scripts/test.py
@@ -28,14 +28,60 @@ model.eval()  # disables dropout for deterministic results
 
 batch_converter = alphabet.get_batch_converter()
 
+def compute_representations(data: list, dest: str = None, device: str = 'cuda'):
+    '''
+    generate sequence representations using esm2_t33_650M_UR50D.
+    The representation are of size 1280.
+
+    Parameters:
+        data (list): list of tuples containing sequence labels (str) and protein sequences
+                     (str or biotite.sequence.ProteinSequence) (label, sequence)
+        dest (str): destination where embeddings are saved. Default None (won't save if dest is None).
+        device (str): device used for calculation or representations. Default "cuda".
+                      other options are "cpu", or "mps" for M1/M2 chip
+
+    Returns: representations (list)
+
+    Example:
+        data = [("protein1", "AGAVCTGAKLI"), ("protein2", "AGHRFLIKLKI")]
+        representations = get_sequence_representations(data)
+
+    '''
+    # check datatype of data
+    if all(isinstance(x[0], str) and isinstance(x[1], str) for x in data):
+        pass # all elements are strings
+    else:
+        data = [(x[0], str(x[1])) for x in data]
+
+    batch_labels, batch_strs, batch_tokens = batch_converter(data)
+    batch_lens = (batch_tokens != alphabet.padding_idx).sum(1)
+
+    with torch.no_grad():
+        results = model(batch_tokens.to(device), repr_layers=[33], return_contacts=True)
+
+    token_representations = results["representations"][33]
+
+    # Generate per-sequence representations via averaging
+    # NOTE: token 0 is always a beginning-of-sequence token, so the first residue is token 1.
+    sequence_representations = []
+    for i, tokens_len in enumerate(batch_lens):
+        sequence_representations.append(token_representations[i, 1: tokens_len - 1].mean(0))
+
+    if dest is not None:
+        for i in range(len(sequence_representations)):
+            _dest = os.path.join(dest, batch_labels[i])
+            torch.save(sequence_representations[i], _dest + '.pt')
+
+    return sequence_representations
+
 PNMT_data = list(zip(PNMT_hits, PNMT_hit_seqs))
 PNMT_representations = []
 for i in range(0, len(PNMT_data), 10):
-    r = embedd.compute_representations(PNMT_data[i:i + batch_size], dest='../example_data/representations/PNMT' ,device=str(device))
+    r = compute_representations(PNMT_data[i:i + batch_size], dest='../example_data/representations/PNMT' ,device=str(device))
     PNMT_representations.append(r)
 
 ASMT_data = list(zip(ASMT_hits, ASMT_hit_seqs))
 ASMT_representations = []
 for i in range(0, len(ASMT_data), 5):
-    r = embedd.compute_representations(ASMT_data[i:i + batch_size], dest='../example_data/representations/PNMT', device=str(device))
+    r = compute_representations(ASMT_data[i:i + batch_size], dest='../example_data/representations/PNMT', device=str(device))
     ASMT_representations.append(r)
