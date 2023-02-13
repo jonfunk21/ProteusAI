06b99a30a899251b4c0960354128633ff5884ee4
jonfunk21 <funk.jonathan21@gmail.com>
2023-02-13 12:55:07 +0100

    Added functionality to write fasta files

--- io_tools/fasta.py
+++ io_tools/fasta.py
@@ -2,7 +2,7 @@ import os
 from biotite.sequence import ProteinSequence
 
 
-def load(path: str) -> list:
+def load(path: str) -> tuple:
     """
     Loads all fasta files from a directory, returns the names/ids and sequences as lists.
 
@@ -28,3 +28,28 @@ def load(path: str) -> list:
                     sequences.append(ProteinSequence(line))
 
     return names, sequences
+
+
+def write(names: str, sequences: str, dest: str = None):
+    """
+    Takes a list of names and sequences and writes a single
+    fasta file containing all the names and sequences. The
+    files will be saved at the destination
+
+    Parameters:
+        names (list): list of sequence names
+        sequences (list): list of sequences
+        dest (str): path to output file
+
+    Example:
+        write_fasta(names, sequences, './out.fasta')
+    """
+    assert len(names) == len(sequences), 'names and sequences must have the same length'
+
+    with open(dest, 'w') as f:
+        for i in range(len(names)):
+            f.writelines('>' + names[i] + '\n')
+            if i == len(names) - 1:
+                f.writelines(sequences[i])
+            else:
+                f.writelines(sequences[i] + '\n')
