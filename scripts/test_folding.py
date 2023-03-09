932238133827cdd2747db439ebb668278fc39212
jonfunk21 <funk.jonathan21@gmail.com>
2023-03-09 16:12:24 +0100

    some bug

--- scripts/test_folding.py
+++ scripts/test_folding.py
@@ -2,5 +2,5 @@ import sys
 sys.path.append('../')
 from sampler import MCMC
 
-Sampler = MCMC.SequenceOptimizer(native_seq='M', n_iter=1000, n_traj=5)
+Sampler = MCMC.SequenceOptimizer(native_seq='LNLKDSIGL', n_iter=1000, n_traj=5)
 Sampler.run()
