c53fbafe18e4dd730c549f7d887475895264206d
jonfunk21 <funk.jonathan21@gmail.com>
2023-03-10 15:13:50 +0100

    mini hallucination experiment

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -54,4 +54,4 @@ pip install 'dllogger @ git+https://github.com/NVIDIA/dllogger.git'
 pip install 'openfold @ git+https://github.com/aqlaboratory/openfold.git@4b41059694619831a7db195b7e0988fc4ff3a307'
 
 cd scripts
-python3  test_folding.py
+python3 test_folding.py
