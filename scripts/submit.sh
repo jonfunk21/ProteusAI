727926052a9e0dd7433a97124137208805ec83ff
jonfunk21 <funk.jonathan21@gmail.com>
2023-03-10 15:40:04 +0100

    mini hallucination experiment

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -49,12 +49,12 @@ pip install 'dllogger @ git+https://github.com/NVIDIA/dllogger.git'
 pip install 'openfold @ git+https://github.com/aqlaboratory/openfold.git@4b41059694619831a7db195b7e0988fc4ff3a307'
 
 cd scripts
-#python3 test_folding.py
+#python3 hallucination.py
 
 
 
 # for visualization with pymol
-module load pymol/1.7.6.0
+module load pymol/2.4.0
 pip3 install Pillow
 pip3 install imageio
 
