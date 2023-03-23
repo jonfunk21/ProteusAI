0eec0e17e607c94edf34071437506e6748e63d20
jonfunk21 <funk.jonathan21@gmail.com>
2023-03-23 10:46:27 +0100

    parallel search, n_traj 20

--- design/MCMC.py
+++ design/MCMC.py
@@ -360,7 +360,7 @@ class ProteinDesign:
 
         # write energy_log in data_out
         if outdir is not None:
-            df = pd.DataFrame(self.energy_log[0])
+            df = pd.DataFrame(self.energy_log)
             df.to_csv(os.path.join(data_out, f'energy_log.pdb'), index=False)
 
         for i in range(steps):
