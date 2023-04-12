1e592b3255e4fefb43e241347790897fc7f4f197
jonfunk21 <funk.jonathan21@gmail.com>
2023-04-12 12:33:51 +0200

    train activity prediction model

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -48,6 +48,6 @@ pip install "fair-esm[esmfold]"
 pip install 'dllogger @ git+https://github.com/NVIDIA/dllogger.git'
 pip install 'openfold @ git+https://github.com/aqlaboratory/openfold.git@4b41059694619831a7db195b7e0988fc4ff3a307'
 
-cd embedd
+cd models
 
-python3 embedd_esm.py -f '../example_data/directed_evolution/GB1/GB1.fasta' -d '../example_data/directed_evolution/GB1/representations' -a True
+python3 train.py
