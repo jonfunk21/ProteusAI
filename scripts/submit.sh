e78249728ab6489b9c74a21a0e4ab0855d961ffd
jonfunk21 <funk.jonathan21@gmail.com>
2023-04-24 08:17:14 +0200

    new LLM tool file

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -49,6 +49,6 @@ pip install "fair-esm[esmfold]"
 pip install 'dllogger @ git+https://github.com/NVIDIA/dllogger.git'
 pip install 'openfold @ git+https://github.com/aqlaboratory/openfold.git@4b41059694619831a7db195b7e0988fc4ff3a307'
 
-cd models
+cd LLM_tools
 
-python3 model_optimization.py
+python3 esm.py
