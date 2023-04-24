23a440ec96a9e77e1054cfa2db19e1c0f30afd0a
jonfunk21 <funk.jonathan21@gmail.com>
2023-04-24 14:45:29 +0200

    Package updates

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ProteusAI",
+    version="0.1.0",
+    author="Jonathan Funk",
+    author_email="funk.jonathan21@gmail.com",
+    description="ProteusAI is a python package designed for AI driven protein engineering.",
+    url="https://github.com/jonfunk21/ProteusAI",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "pandas",
+        "numpy",
+        "requests",
+        "scipy",
+        "fair-esm",
+        "matplotlib",
+        "biopython",
+        "biotite",
+        "scikit-learn",
+        "optuna",
+        "seaborn",
+        "openpyxl",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+)
--- src/poteusAI/__init__.py
+++ src/poteusAI/__init__.py
@@ -1,6 +1,6 @@
 # This source code is part of the proteusAI package and is distributed
 # under the MIT License.
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __name__ = "proteusAI"
 __author__ = "Jonathan Funk"
