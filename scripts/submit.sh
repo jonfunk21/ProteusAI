26a7c08b90aa1c619ae93a750977cbb0d9d69440
jonfunk21 <funk.jonathan21@gmail.com>
2023-04-24 09:51:50 +0200

    renamed llm tools to plm tools

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -49,6 +49,6 @@ pip install "fair-esm[esmfold]"
 pip install 'dllogger @ git+https://github.com/NVIDIA/dllogger.git'
 pip install 'openfold @ git+https://github.com/aqlaboratory/openfold.git@4b41059694619831a7db195b7e0988fc4ff3a307'
 
-cd LLM_tools
+cd plm_tools
 
 python3 esm_tools.py
