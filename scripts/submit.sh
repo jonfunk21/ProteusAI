d3fad8fbf1cb5f70359c7d5ae96a1851d27d65a4
jonfunk21 <funk.jonathan21@gmail.com>
2023-04-25 08:40:43 +0200

    testing esm_tools

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -50,4 +50,4 @@ pip install 'dllogger @ git+https://github.com/NVIDIA/dllogger.git'
 pip install 'openfold @ git+https://github.com/aqlaboratory/openfold.git@4b41059694619831a7db195b7e0988fc4ff3a307'
 
 
-python3 src/proteusAI/plm/esm_tools.py
+python3 ../src/proteusAI/plm/esm_tools.py
