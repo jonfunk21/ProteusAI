044ca0eb77f5e999f68a15bcd4dcd311fd34802b
jonfunk21 <funk.jonathan21@gmail.com>
2023-05-16 10:08:24 +0200

    protein_CLIP compute embeddings test

---
+++ Projects/protein_CLIP/scripts/03_compute_embeddings.py
@@ -0,0 +1,44 @@
+import sys
+sys.path.append('../../../src/')
+from proteusAI.ML.plm.esm_tools import *
+import os
+import pandas as pd
+from transformers import GPT2Tokenizer, GPT2Model
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
+gpt2 = GPT2Model.from_pretrained('gpt2-large')
+gpt2.eval()
+
+script_path = os.path.dirname(__file__)
+data_dir = os.path.join(script_path, '../data')
+df_path = os.path.join(data_dir, 'processed/01_uniprotID_and_EC_raw-checkpoint.csv')
+fasta_dir = os.path.join(data_dir, 'fastas')
+
+df = pd.read_csv(df_path)
+
+fasta_files = [os.path.join(fasta_dir, f) for f in os.listdir(fasta_dir) if f.endswith('.fasta')]
+
+batch_size=10
+for i in range(0, len(fasta_files), batch_size):
+    names = [n.split('/')[-1][:-6] for n in fasta_files[i:i + batch_size]]
+    seqs = []
+    for f in fasta_files[i:i + batch_size]:
+        _, s = fasta.load_fasta(f)
+        seqs.append(s[0])
+
+    # calculate embeddings of name
+    text_embeddings = []
+    for n in names:
+        row = df[df['protein'] == n]
+        text = '; '.join(f'{key}: {value}' for key, value in row.items() if key in ['EC', 'DE', 'AN', 'CA', 'CF', 'CC'])
+        encoded_input = tokenizer(text, return_tensors='pt')
+        output = gpt2(**encoded_input)
+        text_embedding = output['last_hidden_state'][i, :].mean(0)
+        text_embeddings.append(text_embedding)
+        torch.save(text_embedding, f'../data/embeddings/descriptions{n}.pt')
+
+    results, batch_lens, batch_labels, alphabet = esm_compute(seqs)
+    sequence_representations = get_seq_rep(results, batch_lens)
+    for i in range(len(batch_lens)):
+        torch.save(sequence_representations[i,:], f'../data/embeddings/proteins{n}.pt')
+    asdf
