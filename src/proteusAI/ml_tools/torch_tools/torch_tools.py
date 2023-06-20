e297765d7f80da13b7a5569ea4cb5fbef99a3a29
jonfunk21 <funk.jonathan21@gmail.com>
2023-06-20 11:12:41 +0200

    added blosum encoding to torch_tools

--- src/proteusAI/ml_tools/torch_tools/torch_tools.py
+++ src/proteusAI/ml_tools/torch_tools/torch_tools.py
@@ -54,3 +54,62 @@ def one_hot_decoder(tensor: torch.Tensor, alphabet: dict):
         decoded_sequences.append(''.join(decoded_sequence))
 
     return decoded_sequences
+
+
+def blosum_encoding(sequence, matrix='BLOSUM62', canonical=True):
+    '''
+    Returns BLOSUM encoding for amino acid sequence. Unknown amino acids will be
+    encoded with 0.5 at in entire row.
+
+    Parameters:
+    -----------
+        sequence (str): Amino acid sequence
+        blosum_matrix_choice (str): Choice of BLOSUM matrix. Can be 'BLOSUM50' or 'BLOSUM62'
+        canonical (bool): only use canonical amino acids
+
+    Returns:
+    --------
+        torch.Tensor: BLOSUM encoded sequence
+    '''
+
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    ### Amino Acid codes
+    alphabet_file = os.path.join(script_dir, "matrices/alphabet")
+    alphabet = np.loadtxt(alphabet_file, dtype=str)
+
+    # Define BLOSUM matrices
+    _blosum50 = np.loadtxt(os.path.join(script_dir, "matrices/BLOSUM50"), dtype=float).reshape((24, -1)).T
+    _blosum62 = np.loadtxt(os.path.join(script_dir, "matrices/BLOSUM62"), dtype=float).reshape((24, -1)).T
+
+    # Choose BLOSUM matrix
+    if matrix == 'BLOSUM50':
+        matrix = _blosum50
+    elif matrix == 'BLOSUM62':
+        matrix = _blosum62
+    else:
+        raise ValueError("Invalid BLOSUM matrix choice. Choose 'BLOSUM50' or 'BLOSUM62'.")
+
+    blosum_matrix = {}
+    for i, letter_1 in enumerate(alphabet):
+        if canonical:
+            blosum_matrix[letter_1] = matrix[i][:20]
+        else:
+            blosum_matrix[letter_1] = matrix[i]
+    
+    # create empty encoding vector
+    encoding = np.zeros((len(sequence), len(blosum_matrix['A'])))
+    
+    # Convert each amino acid in sequence to BLOSUM encoding
+    for i, aa in enumerate(sequence):
+        if aa in alphabet:
+            encoding[i, :] = blosum_matrix[aa]
+        else:
+            # Handle unknown amino acids with a default value of 0.5
+            encoding[i, :] = 0.5
+
+    # Convert numpy array to torch tensor
+    encoding = torch.from_numpy(encoding)
+
+    return encoding
