f5bea2a869f3ea57bebe92f46ef3d80939f61bf6
jonfunk21 <funk.jonathan21@gmail.com>
2023-06-22 15:43:57 +0200

    calculate missing representations

--- projects/activity_prediction/submit.sh
+++ projects/activity_prediction/submit.sh
@@ -52,7 +52,7 @@ pip install 'openfold @ git+https://github.com/aqlaboratory/openfold.git@4b41059
 cd ~/projects/proteusAI/projects/activity_prediction
 
 
-python3 train_VAE.py --encoder OHE --epochs 10000 
-python3 train_VAE.py --encoder BLOSUM62 --epochs 10000
-python3 train_VAE.py --encoder BLOSUM50 --epochs 10000
-#python3 compute_representations.py
+python3 compute_representations.py
+#python3 train_VAE.py --encoder OHE --epochs 10000 
+#python3 train_VAE.py --encoder BLOSUM62 --epochs 10000
+#python3 train_VAE.py --encoder BLOSUM50 --epochs 10000
