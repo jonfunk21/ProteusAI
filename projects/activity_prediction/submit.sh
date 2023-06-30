31132e38df870356c7068a75986216bf67a73d77
jonfunk21 <funk.jonathan21@gmail.com>
2023-06-30 22:54:22 +0200

    compute representations

--- projects/activity_prediction/submit.sh
+++ projects/activity_prediction/submit.sh
@@ -53,7 +53,7 @@ cd ~/projects/proteusAI/projects/activity_prediction
 
 
 python3 compute_representations.py --model esm2
-python3 train_esm_regressor.py --model esm2
+#python3 train_esm_regressor.py --model esm2
 #python3 train_VAE.py --encoder OHE --epochs 10000 
 #python3 train_VAE.py --encoder BLOSUM62 --epochs 10000
 #python3 train_VAE.py --encoder BLOSUM50 --epochs 10000
