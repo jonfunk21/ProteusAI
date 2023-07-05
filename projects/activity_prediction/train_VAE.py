bc5f891da1778685b94ce0a8e5327d70877fa241
jonfunk21 <funk.jonathan21@gmail.com>
2023-07-05 10:51:24 +0200

    removed dropouts, added annotation

--- projects/activity_prediction/train_VAE.py
+++ projects/activity_prediction/train_VAE.py
@@ -15,13 +15,26 @@ import argparse
 parser = argparse.ArgumentParser(description="Process some strings.")
 parser.add_argument('--encoder', type=str, default='OHE', help='choose encoding method amino acid sequences ["OHE", "BLOSUM62", "BLOSUM50"]')
 parser.add_argument('--epochs', type=int, default=1000)
+parser.add_argument('--z_dim', type=int, default=64)
+parser.add_argument('--dropout_p', type=float, default=0.0)
+parser.add_argument('--hidden_layers', type=str, default='2048,1024,256', help='Comma-separated list of hidden layer sizes')
 parser.add_argument('--save_checkpoints', dest='save_checkpoints', action='store_true', help='Save checkpoint during the process')
 parser.set_defaults(save_checkpoint=False)
 parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
+parser.add_argument('--weight_decay', type=float, default=1e-5)
+parser.add_argument('--step_size', type=int, default=1000, help='Stepsize for learning rate scheduler')
+parser.add_argument('--gamma', type=float, default=0.1, help='Gamma value for learning rate scheduler')
 args = parser.parse_args()
 
+# Hyperparameters
 epochs = args.epochs
 save_checkpoints = args.save_checkpoints
+dropout_p = args.dropout_p
+hidden_layers = [int(x) for x in args.hidden_layers.split(',')]
+z_dim = args.z_dim
+weight_decay = args.weight_decay
+gamma = args.gamma
+step_size = args.step_size
 
 # encoding type ohe, BLOSUM62 or BLOSUM50
 encoding_type = args.encoder
@@ -66,8 +79,8 @@ for name in names:
     seq_len, alphabet_size = train_data.dataset[0].shape
     
     # Initialize model, optimizer and epochs
-    model = Autoencoders.VAE(input_dim=seq_len * alphabet_size, hidden_dims=[2048, 1024, 256], z_dim=64, dropout=0.1).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-5)
+    model = Autoencoders.VAE(input_dim=seq_len * alphabet_size, hidden_dims=hidden_layers, z_dim=z_dim, dropout=dropout_p).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=weight_decay)
     scheduler = StepLR(optimizer, step_size=1000, gamma=0.1)
 
     # Train the model on the dataset
