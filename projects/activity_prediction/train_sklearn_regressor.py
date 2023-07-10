f301d0ed55cccf9e7f5064d7164068123de3687c
jonfunk21 <funk.jonathan21@gmail.com>
2023-07-10 09:17:15 +0200

    changed file names

--- projects/activity_prediction/train_sklearn_regressor.py
+++ projects/activity_prediction/train_sklearn_regressor.py
@@ -89,21 +89,21 @@ for name in names:
     predictions = best_model.predict(X_test)
 
     # plot best predictions
-    sklearn_tools.plot_predictions_vs_groundtruth(y_test, predictions, fname=f'{results_plots_path}/{name}_pred_vs_true.png')
+    sklearn_tools.plot_predictions_vs_groundtruth(y_test, predictions, fname=f'{results_plots_path}/{name}_{encoding_type}_pred_vs_true.png')
 
     # Save the best model to a file
-    dump(best_model, f'{checkpoints_path}/{name}_best_model.joblib')
+    dump(best_model, f'{checkpoints_path}/{name}_{encoding_type}_best_model.joblib')
 
     # save results
     resutls = {'name':model_name, 'test_r2':test_r2, 'corr_coef':corr_coef}
-    with open(f'{results_path}/{model_name}_results.json', 'w') as f:
+    with open(f'{results_path}/{name}_{encoding_type}_results.json', 'w') as f:
         json.dump(resutls, f)
 
 
     # Save the best parameters to a JSON file
-    with open(f'{results_path}/{model_name}_best_params.json', 'w') as f:
+    with open(f'{results_path}/{name}_{encoding_type}_best_params.json', 'w') as f:
         json.dump(best_params_, f)
 
     # Append predictions to test_df and save
     test_df['predictions'] = predictions
-    test_df.to_csv(f'{results_path}/{model_name}_predictions.csv', index=False)
+    test_df.to_csv(f'{results_path}/{name}_{encoding_type}_predictions.csv', index=False)
