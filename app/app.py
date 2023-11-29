f0682d2acf4ba1cb22323e2db7f4e331c7768dbb
jonfunk21 <funk.jonathan21@gmail.com>
2023-11-29 15:26:41 +0100

    further development of UI

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import shiny
 from shiny import App, ui, render, Inputs, Outputs, Session, reactive
-from shiny.types import FileInfo
+from shiny.types import FileInfo, ImgData
 import pandas as pd
 import sys
 sys.path.append('src/')
@@ -8,21 +8,23 @@ import proteusAI as pai
 import os
 
 app_ui = ui.page_fluid(
-    ui.panel_title("ProteusAI"),
-
+    
+    #ui.panel_title("ProteusAI"),
+    ui.output_image("image", inline=True, ),
+    
     ui.navset_tab_card(
 
-        
         ui.nav(
             "Data", 
             
             ui.layout_sidebar(
                 ui.panel_sidebar(
-                    # CHANGE THIS TO EMPTY STRING LATER
+                    
                     ui.navset_tab(
                         ui.nav("Load Data",
-                               ui.input_file(id="dataset_file", label="Select dataset", accept=['.csv', '.xlsx', '.xls', '.fasta']),
+                               ui.input_file(id="dataset_file", label="Select dataset", accept=['.csv', '.xlsx', '.xls', '.fasta'], placeholder="None"),
                                
+                               # CHANGE THIS TO EMPTY STRING LATER
                                ui.input_text(id="project_path", label="Project Path", value="demo/example_project"),
                                
                                "Data selection",
@@ -39,13 +41,15 @@ app_ui = ui.page_fluid(
                                    ),
                                ),
                     
-                               ui.input_action_button(id='compute_library', label='Confirm Selection', class_="btn-success"),
+                               ui.input_action_button('compute_library', 'Confirm Selection', class_="btn-success"),
+                               
                                
                         ),
                         ui.nav("Custom Protein", "Under construction..."),
                     )
                 ),
                 ui.panel_main(
+                    
                     "Raw data view",
                     #ui.panel_conditional(
                     #    "input.dataset_table === null",
@@ -58,20 +62,77 @@ app_ui = ui.page_fluid(
         
         ),
 
-        ui.nav("Library", "tab a content"),
+        ui.nav("Library", 
+               
+               ui.layout_sidebar(
+                   ui.panel_sidebar(
+                       ui.navset_tab(
+                           ui.nav("Visualize",
+                                ui.input_select("vis_method","Visualization Method",["t-SNE", "PCA"]),
+                                ui.input_select("color_by", "Color by", ["Y-value", "Site", "Custom"])
+                           ),
+                           ui.nav("Edit",
+                               
+                           )
+                       )
+                   ),
+                   ui.panel_main(
+                       
+                   )
+               )
+        ),
 
-        ui.nav("Model", "tab a content"),
+        ui.nav("Model", 
+                ui.layout_sidebar(
+                    ui.panel_sidebar(
+                        ui.navset_tab(
+                            ui.nav("Supervised",
+                                   "Model Customization",
+                                   ui.row(
+                                       ui.column(6,
+                                            ui.input_select("model_type", "Model type", ["Random Forrest", "KNN", "SVM", "FFNN"])
+                                        ),
+                                        ui.column(6,
+                                            ui.input_select("model_task", "Model task", ["Regression", "Classification"])
+                                        ),
+                                        ui.column(6,
+                                            ui.input_select("model_rep_type", "Representaion type", ["ESM-2", "ESM-1v", "One-hot", "BLOSUM50", "BLOSUM62"]),
+                                            "100 % computed"
+                                        ),
 
-        ui.nav_menu(
-            "Results",
-
-            ui.nav("Option 1", "Option 1 content"),
-            
-            align="right",
+                                        ui.column(6,
+                                            ui.input_action_button("compute_reps", "Compute", class_="btn-success"),
+                                            style='padding:25px;'
+                                        )
+                                   ),
+                                   "Choose model parameters"
+                                   
+                            ),
+                            ui.nav("Zero-shot",
+                                   "Model Customization",
+                                   ui.row(
+                                        ui.column(6,
+                                            ui.input_select("zs_model", "Choose zero-shot model", ["ESM-2", "ESM-1v", "MSA-Transformer"])
+                                        ),
+                                        ui.column(6,
+                                            ui.input_select("add_input", "Aditional input (e.g., MSA)", ["None", "MSA"])
+                                        )
+                                   )
+                                   
+                            ),
+                            ui.nav(
+                                "Load model"
+                            )
+                        )
+                    ),
+                ui.panel_main(
+                    "Here you will be able to inspect your model results..."
+                )
+                )
         ),
 
         ui.nav_menu(
-            "Visualization",
+            "Results",
 
             ui.nav("Option 1", "Option 1 content"),
             
@@ -89,6 +150,16 @@ app_ui = ui.page_fluid(
 )
 
 def server(input: Inputs, output: Outputs, session: Session):
+
+    # App logo
+    @output
+    @render.image
+    def image():
+        from pathlib import Path
+
+        dir = Path(__file__).resolve().parent
+        img: ImgData = {"src": str(dir / "logo.png"),  "height": "75px"}
+        return img
 
     # dummy dataset until real dataset is entere
     dataset = reactive.Value(pd.DataFrame(data={
@@ -124,7 +195,7 @@ def server(input: Inputs, output: Outputs, session: Session):
 
         ui.update_select(
             "seq_col",
-            label=f"Select Sequences)",
+            label="Select Sequences",
             choices=cols,
             selected=cols[0],
         )
@@ -150,7 +221,12 @@ def server(input: Inputs, output: Outputs, session: Session):
         else:
             y_type = "class"
 
-        lib.read_data(data=dataset_path(), seqs=input.seq_col(), y=input.y_col(), y_type=y_type, names='Description')
+        # TRY TO FIND A MORE STABLE SOLUTION HERE
+        try:
+            lib.read_data(data=dataset_path(), seqs=input.seq_col(), y=input.y_col(), y_type=y_type, names='Description')
+        except:
+            pass
+
         library.set(lib)
         
 
