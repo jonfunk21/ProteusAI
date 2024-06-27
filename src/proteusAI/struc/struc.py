8c46022d79391547d3fef787740fdf5148027b40
jonfunk21 <funk.jonathan21@gmail.com>
2024-06-27 10:57:29 +0000

    change of 3D visualization option for individual chains

--- src/proteusAI/struc/struc.py
+++ src/proteusAI/struc/struc.py
@@ -312,8 +312,9 @@ def show_pdb(pdb_path, color='confidence', vmin=50, vmax=90, chains=None, Ls=Non
     This function displays the 3D structure of a protein from a given PDB file in a Jupyter notebook.
     The protein structure can be colored by chain, rainbow, pLDDT, or confidence value. The size of the
     display can be changed. The sidechains and mainchains can be displayed or hidden.
-    Additional functionality includes highlighting specific residues by passing their numbers in a list,
-    and displaying ligands as sticks and ions as spheres.
+    Additional functionality includes highlighting specific residues within specific chains by passing
+    a dictionary with chains as keys and lists of residue numbers as values, and displaying ligands
+    as sticks and ions as spheres.
     Parameters:
         pdb_path (str): The filename of the PDB file that contains the protein structure.
         color (str, optional): The color scheme for the protein structure. Can be "chain", "rainbow", "pLDDT", "confidence". Defaults to "rainbow".
@@ -324,7 +325,7 @@ def show_pdb(pdb_path, color='confidence', vmin=50, vmax=90, chains=None, Ls=Non
         size (tuple, optional): The size of the display window. Defaults to (800, 480).
         show_sidechains (bool, optional): Whether to display the sidechains. Defaults to False.
         show_mainchains (bool, optional): Whether to display the mainchains. Defaults to False.
-        highlight (list, optional): A list of residue numbers to be highlighted. Defaults to None.
+        highlight (dict, optional): A dictionary with chains as keys and lists of residue numbers to be highlighted as values. Defaults to None.
         sticks (list, optional): A list of residues that should be displayed as sticks. Default to None.
     Returns:
         view: The 3Dmol view object that displays the protein structure.
@@ -346,15 +347,12 @@ def show_pdb(pdb_path, color='confidence', vmin=50, vmax=90, chains=None, Ls=Non
         # Set default style as white
         view.setStyle({}, {'cartoon': {'color': 'white'}})
     
-    # Highlight specific residues
-    if highlight:
-        for resi in highlight:
-            if resi in sticks:
-                highlight_style = {'stick': {'colorscheme': 'blueCarbon', 'radius': 0.3}}
-                sticks.remove(resi)
-            else:
-                highlight_style = {'cartoon': {'colorscheme': 'blueCarbon', 'radius': 0.3}}
-            view.addStyle({'resi': str(resi)}, highlight_style)
+    # Highlight specific residues per chain
+    if highlight and isinstance(highlight, dict):
+        for chain, residues in highlight.items():
+            for resi in residues:
+                highlight_style = {'stick' if resi in (sticks or []) else 'cartoon': {'colorscheme': 'blueCarbon', 'radius': 0.3}}
+                view.addStyle({'chain': chain, 'resi': str(resi)}, highlight_style)
     
     if sticks:
         stick_style = {'stick': {'radius': 0.3}}
@@ -381,6 +379,7 @@ def show_pdb(pdb_path, color='confidence', vmin=50, vmax=90, chains=None, Ls=Non
     view.zoomTo()
 
     return view
+
 
 
 def relax_pdb(file, dest='outputs/struc/relaxed'):
