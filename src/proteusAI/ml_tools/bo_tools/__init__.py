9d50d400bd06d46064ec7aada963d88f93631235
jonfunk21 <funk.jonathan21@gmail.com>
2024-07-29 12:23:31 +0000

    added acquisition functions

---
+++ src/proteusAI/ml_tools/bo_tools/__init__.py
@@ -0,0 +1,11 @@
+# This source code is part of the proteusAI package and is distributed
+# under the MIT License.
+
+"""
+A subpackage for bayesian optimization tools.
+"""
+
+__name__ = "proteusAI"
+__author__ = "Jonathan Funk"
+
+from .acq_fn import *
---
+++ src/proteusAI/ml_tools/bo_tools/acq_fn.py
@@ -0,0 +1,8 @@
+# This source code is part of the proteusAI package and is distributed
+# under the MIT License.
+
+__name__ = "proteusAI"
+__author__ = "Jonathan Funk"
+
+def greedy(mean, sigma=None, best_y=None, xi=0.01):
+    return mean
