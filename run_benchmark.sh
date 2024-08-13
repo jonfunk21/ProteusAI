218c89bf8a0dc4a420b0ba5d8681d020c5cb1879
jonfunk21 <funk.jonathan21@gmail.com>
2024-08-13 08:32:19 +0000

    run benchmarks

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -3,12 +3,15 @@
 # Define the possible combinations of models and embeddings
 models=("gp" "rf" "svm" "knn")
 embeddings=("esm2" "ohe" "blosum62" "blosum50")
+acq_fns=("ei" "ucb" "greedy" "random")
 
 # Loop over each model
 for model in "${models[@]}"; do
   # Loop over each embedding
   for emb in "${embeddings[@]}"; do
-    # Execute the script with the current combination of model and embedding
-    python demo/MLDE_benchmark.py --model "$model" --emb "$emb"
+    for acq_fn in "${acq_fns[@]}"; do
+      # Execute the script with the current combination of model and embedding
+      python demo/MLDE_benchmark.py --model "$model" --emb "$emb" --acquisition_fn "$acq_fn"
+    done
   done
 done
