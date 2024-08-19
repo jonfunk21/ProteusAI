f0552a8d967382b692ce7a5e23a65538328bdb1c
jonfunk21 <funk.jonathan21@gmail.com>
2024-08-19 09:16:59 +0000

    changed max itteration initial value

--- demo/MLDE_benchmark.py
+++ demo/MLDE_benchmark.py
@@ -18,7 +18,7 @@ parser.add_argument('--rep', type=str, default='esm2', help='Representation type
 parser.add_argument('--zs-model', type=str, default='esm1v', help='Zero-shot model name.')
 parser.add_argument('--benchmark-folder', type=str, default='demo/demo_data/DMS/', help='Path to the benchmark folder.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-parser.add_argument('--max-iter', type=int, default=100, help='Maximum number of iterations (None for unlimited).')
+parser.add_argument('--max-iter', type=int, default=20, help='Maximum number of iterations (None for unlimited).')
 parser.add_argument('--device', type=str, default='cuda', help='Device to run the model on (e.g., cuda, cpu).')
 parser.add_argument('--batch-size', type=int, default=1, help='Batch size for processing.')
 parser.add_argument('--improvement', type=str, nargs='+', default=[5, 10, 20, 50, 'improved'], help='List of improvements.')
@@ -181,7 +181,6 @@ fastas.sort()
 first_discovered_data = {}
 for i in range(len(datasets)):
     for N in SAMPLE_SIZES:
-        print(ACQ_FN)
         d = os.path.join(BENCHMARK_FOLDER, datasets[i])
         f = os.path.join(BENCHMARK_FOLDER, fastas[i])
         name = datasets[i][:-4]
