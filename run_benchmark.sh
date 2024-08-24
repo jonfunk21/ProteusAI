a36847c46b3b724a831eaa30d74458cc63d48444
jonfunk21 <funk.jonathan21@gmail.com>
2024-08-24 07:10:09 +0000

    run models first

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define the possible combinations of models and embeddings
-representations=("esm2" "ohe" "blosum62") # "blosum50"
-models=("gp" "rf") # "svm" "knn"
-acq_fns=("ei" "ucb" "greedy") # "random"
+representations=("esm2" "blosum62" "ohe") # "blosum50"
+acq_fns=("ei" "greedy" "ucb") # "random"
+models=("ridge") # "gp" "rf" "ridge" "svm" "knn"
 
 for acq_fn in "${acq_fns[@]}"; do
-  for model in "${models[@]}"; do
-    for rep in "${representations[@]}"; do
+  for rep in "${representations[@]}"; do
+    for model in "${models[@]}"; do
       # Execute the script with the current combination of model and embedding
       python demo/MLDE_benchmark.py --model "$model" --rep "$rep" --acquisition_fn "$acq_fn" --max-iter 100
       python demo/plot_benchmark.py --rep "$rep" --max-sample 100 --model "$model" --acquisition-fn "$acq_fn"
