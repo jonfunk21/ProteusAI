d4f808743b522025cc38ad8c4f6d96e57d29df8a
jonfunk21 <funk.jonathan21@gmail.com>
2024-11-09 23:31:57 +0000

    demo update

--- demo/demo.py
+++ demo/demo.py
@@ -1,41 +1,36 @@
 import os
 print(os.getcwd())
-
 import sys
 sys.path.append('src/')
 import proteusAI as pai
 
+
 # will initiate storage space - else in memory
-#library = pai.Library(user='guest') 
+datasets = ['demo/demo_data/Nitric_Oxide_Dioxygenase_raw.csv']
+y_columns = ['Data'] # enter your names here, e.g. 'pae1', 'plddt1'
 
-# load data from csv or excel: x should be sequences, y should be labels, y_type class or num
-library = pai.Library(user='guest', source='demo/demo_data/Nitric_Oxide_Dioxygenase_raw.csv', seqs_col='Sequence', y_col='Description', 
-                    y_type='class', names_col='Description')
+results_dictionary = {}
+for dataset in datasets:
+    for y in y_columns:
+        # load data from csv or excel: x should be sequences, y should be labels, y_type class or num
+        library = pai.Library(source=dataset, seqs_col='Sequence', y_col=y, 
+                            y_type='num', names_col='Description')
 
 
-# compute and save ESM-2 representations at example_lib/representations/esm2
-library.compute(method='esm2', batch_size=10)
+        # compute and save ESM-2 representations at example_lib/representations/esm2
+        library.compute(method='esm2', batch_size=10)
 
-# collect the top 5 proteins of the library
-top_5 = library.top_n(n=5)
 
-# fold the top proteins
-dest = os.path.join(library.user, library.file, "protein")
+        # define a model
+        model = pai.Model(library=library, k_folds=5, model_type='rf', x='blosum62')
 
-for top_i in top_5:
-    top_i.esm_fold(dest=dest)
 
-# perform structure based design
-dest = os.path.join(library.user, library.file, "design")
-designs = [top_i.esm_if(num_samples=2, dest=dest) for top_i in top_5]
+        # train model
+        model.train()
 
-# do zero shot predictions for the proteins
-design_libs = {}
-for i, design in enumerate(designs):
-    lib_path = os.path.join(design[:-4], "library") # create custom library path
-    lib = pai.Library(user='guest')
-    lib.read_data(design, seqs='sequence', names='seqid', y='log_likelihood', y_type='num', lib_path=lib_path)
-    design_libs[i] = lib
-    
-design_libs
-# visualize the protein islands with UMAP
+        # search for new mutants
+        out = model.search(optim_problem='max')
+
+
+        # df = out['df']
+        # df.to_csv('<your results path>')
