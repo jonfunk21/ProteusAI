51c36b10a18b17cfba38fcf7b89968102a1a6711
jonfunk21 <funk.jonathan21@gmail.com>
2024-11-21 09:59:05 +0000

    BugFix in protein.py 'filter_solvent'

--- src/proteusAI/Protein/protein.py
+++ src/proteusAI/Protein/protein.py
@@ -9,6 +9,7 @@ import sys
 import warnings
 from typing import Union
 
+import biotite.structure
 import pandas as pd
 import torch
 import biotite
@@ -254,7 +255,7 @@ class Protein:
         prot = struc.load_struc(prot_f)
 
         if filter_solvent:
-            non_solvent_mask = ~struc.filter_solvent(prot)
+            non_solvent_mask = biotite.structure.filter_solvent(prot)
             prot = prot[non_solvent_mask]
 
         seqs = struc.get_sequences(prot_f)
